d7ce4643784bc37ae9b1da602a05d91baa710294
s_130 <ca01234009@st.kawahara.ac.jp>
2025-07-25 13:06:22 +0900

    PlayerのHPが0の場合、ゲームオーバー画面表示、0以上の場合、非表示

--- Assets/Scripts/HPManager.cs
+++ Assets/Scripts/HPManager.cs
@@ -5,36 +5,34 @@ using UnityEngine.UI;
 
 public class HPManager : MonoBehaviour
 {
-    public Image m_healImage;
-    [SerializeField]
-    private int m_maxHP;
-    [SerializeField]
-    private int m_minHP;
-    private int m_hp;
-    public bool m_isHpZero = false;
+    public static HPManager m_instance;
+    public Image m_healImage;           //HPバーのUI
 
+    [SerializeField]
+    private int m_maxHP = 100;          //最大HP
+    [SerializeField]
+    private int m_minHP = 0;            //最小HP
+    private int m_hp;                   //現在のHP
+    public bool m_isHpZero = false;     //HPが0になったか
+
+
+    void Awake()
+    {
+        if(m_instance == null)
+        {
+            m_instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        m_hp = m_maxHP;
+        m_hp = m_maxHP; // 初期HPを最大HPに設定
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(m_hp < m_minHP)
-        {
-            Debug.Log("ゲームオーバー");
-            m_isHpZero = true;
-
-            m_hp = m_minHP;
-        }
-
-        if(m_hp> m_maxHP)
-        {
-            m_hp = m_maxHP;
-        }
-
+        
         if(Input.GetMouseButtonDown(2))
         {
             Damage(10);
@@ -50,12 +48,36 @@ public class HPManager : MonoBehaviour
     public void Damage(int damage)
     {
         m_hp -= damage;
-        m_healImage.fillAmount = (float)m_hp / m_maxHP;
+        UpdateHPUI();
+
+        if (m_hp <= m_minHP)    //HPが最小値以下になったら
+        {
+            
+            Debug.Log("ゲームオーバー");
+            m_isHpZero = true;
+            //HPが最小値を下回らないように
+            m_hp = m_minHP;
+        }
     }
 
     public void Heal(int heal)
     {
         m_hp += heal;
+        UpdateHPUI();
+
+        if (m_hp > m_minHP)
+        {
+            m_isHpZero = false;
+        }
+
+        if (m_hp > m_maxHP)
+        {
+            m_hp = m_maxHP;
+        }
+    }
+
+    private void UpdateHPUI()
+    {
         m_healImage.fillAmount = (float)m_hp / m_maxHP;
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+   // public GameObject PanelUIObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            //ÉpÉlÉãÇè¡Ç∑
+           // PanelUIObj.SetActive(false);
+        }
+        
+    }
+}
