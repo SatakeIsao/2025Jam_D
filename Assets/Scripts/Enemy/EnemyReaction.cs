93f484795109fd390a21a2898554a62524c8069d
永見凜 <jijimomo0818@docomo.ne.jp>
2025-07-28 15:38:14 +0900

    弱点追加

--- Assets/Scripts/Enemy/EnemyReaction.cs
+++ Assets/Scripts/Enemy/EnemyReaction.cs
@@ -1,17 +1,54 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+/// <summary>
+/// 弱点の位置
+/// </summary>
+enum EnWeakPointPos {
+    enTop,        // 上
+    enButton,     // 下
+    enLeft,       // 左
+    enRight,      //右
+    enEmpty,      // 空
+}
+
 
 public class EnemyReaction : MonoBehaviour
 {
+    //敵の弱点の位置
+    [SerializeField] private EnWeakPointPos weakPointPos = EnWeakPointPos.enEmpty;    
+
     EnemyStatus enemyStatus; // 敵のステータス
+    WeakPoint weakPoint; // 弱点のスクリプト
+    GameObject m_weakObject = null; // 弱点のオブジェクト    
+
+    private Dictionary<EnWeakPointPos, Vector2> weakPointPattern = new Dictionary<EnWeakPointPos, Vector2>() {
+        { EnWeakPointPos.enTop,    new Vector2(0.0f, 10.0f) },
+        { EnWeakPointPos.enButton, new Vector2(0.0f, -1.0f) },
+        { EnWeakPointPos.enLeft,   new Vector2(-1.0f, 0.0f) },
+        { EnWeakPointPos.enRight,  new Vector2(1.0f, 0.0f) },
+        { EnWeakPointPos.enEmpty,  new Vector2(0.0f, 0.0f) },
+    };
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //弱点生成
+        //if (weakPointPos != EnWeakPointPos.enEmpty)
+        //{
+        //    m_weakObject = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/prefabs/WeakPoint.prefab");
+        //    Instantiate(m_weakObject, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        //    m_weakObject.transform.position += (Vector3)weakPointPattern[weakPointPos]; // 弱点の位置を設定
+        //}
+
         enemyStatus = GetComponent<EnemyStatus>();
+        m_weakObject=GetComponentInChildren<WeakPoint>().gameObject; // 弱点のオブジェクトを取得
+        weakPoint = m_weakObject.GetComponentInChildren<WeakPoint>();
+        weakPoint.transform.position = transform.position + (Vector3)weakPointPattern[weakPointPos]; // 弱点の位置を設定
     }
 
     // Update is called once per frame
@@ -26,10 +63,21 @@ public class EnemyReaction : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject other = collision.gameObject;
-        if (other.tag == "Player") {
-            enemyStatus.ApplyDamage(200); // プレイヤーの攻撃が当たったらダメージを与える
+        if (other.tag == "Player")
+        {
+            if (weakPoint.IsHit)
+            {
+                enemyStatus.ApplyDamage(5 * 2); // 弱点にヒットしているなら大ダメージを与える
+                Debug.Log("2");
+            }
+            else
+            {
+                enemyStatus.ApplyDamage(5); // 弱点にヒットしていないなら通常ダメージを与える
+                Debug.Log("1");
+            }
         }
     }
+
 
     /// <summary>
     /// 死亡判定
