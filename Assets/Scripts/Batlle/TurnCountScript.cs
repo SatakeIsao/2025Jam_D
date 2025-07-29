ae9db54b6a3cef4a1439137b509ac4816cc4a80a
HyouHayashi <CA01244023@st.kawahara.ac.jp>
2025-07-29 17:47:58 +0900

    BattleManagerの追加

--- Assets/Scripts/Batlle/TurnCountScript.cs
+++ Assets/Scripts/Batlle/TurnCountScript.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class TurnCountScript : MonoBehaviour
 {
-    [SerializeField] private PlayerMoveBase playerMoveBase; // プレイヤーの移動スクリプトを参照するための変数
+    [SerializeField] private BattleManager battleManager; // プレイヤーの移動スクリプトを参照するための変数
 
     public int EnemyTurnCount = 1; // 敵のターン数を管理する変数
-    public int EnemyTurn = 1; // 敵のターン数を表示する変数
-    public float enemyAttackTimer = 2.0f; //仮置き
-                                          //
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,23 +17,14 @@ public class TurnCountScript : MonoBehaviour
     // ターン数を増やすメソッド
     void Turn()
     {
-        if (playerMoveBase.HasStoppedAfterPull())
+        if(battleManager.IsPlayerActive==false)
         {
-            // プレイヤーが移動を停止した場合、敵のターン数を減らす
-            EnemyTurn = Mathf.Max(EnemyTurn, 0); // 敵のターン数が0未満にならないようにする
-            EnemyTurn =EnemyTurnCount - 1; // 敵のターン数を減らす
-            EnemyTurn = EnemyTurnCount; // 敵のターン数を更新
-            if (EnemyTurn <= 0)
+            EnemyTurnCount -= 1; // 敵のターン数を減らす
+            if (EnemyTurnCount <= 0)
             {
-                Debug.Log("敵のターンです。");
-                EnemyTurnCount = 1; // 敵のターン数をリセット
+                battleManager.IsEnemyActive = true; // 敵のターンを終了
+                EnemyTurnCount = 1; // ターン数をリセット
             }
-
-            else
-            {
-                Debug.Log("敵の攻撃まで後:" + EnemyTurn);
-            }
-
         }
     }
 
